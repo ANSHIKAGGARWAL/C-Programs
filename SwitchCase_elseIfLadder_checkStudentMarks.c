adb0bb57a6daf55c5e377fa763a7b5ae3951d706
Anshika Aggarwal <121662708+ANSHIKAGGARWAL@users.noreply.github.com>
2023-01-20 23:31:58 +0530

    Update and rename elseIfLadder_checkStudentMarks.c to SwitchCase_elseIfLadder_checkStudentMarks.c

--- SwitchCase_elseIfLadder_checkStudentMarks.c
+++ SwitchCase_elseIfLadder_checkStudentMarks.c
@@ -29,6 +29,39 @@ int main() {
 /* If the marks entered by the user is a valid mark between 0 and 500 then ‘grade’ would not be equal to ‘X’ i.e. he has been assigned a valid grade.  */
     if (grade != ‘X’)
         printf("\nYour grade is: %c", grade);
+    
+    
+    // WAP to provide appropriate feedback to a student based on grade secured in the exam.
+    // Code challenge solution
 
+    char grade;
+    puts("Please enter grade: ");
+    scanf("%c", &grade);
+
+    switch (grade) {
+        case 'A':
+            puts("Excellent! Keep it up.");
+            break;
+        case 'B':
+            puts("Very good! Keep it up.");
+            break;
+        case 'C':
+            puts("Good! Try improving in the next exam.");
+            break;
+        case 'D':
+            puts("Average! Need to work hard.");
+            break;
+        case 'E':
+            puts("Poor! Need a lot of hard work.");
+            break;
+        case 'F':
+            puts("Failed! You need to attend special classes after school.");
+            break;
+        default:
+            puts("Invalid grade.");
+    }
+
+    return 0;
+}
     return 0;
 }
